693f2f648199ac220248883a6549c3feeed37f56
RND-23 <ronaldnaupa123@gmail.com>
2025-10-22 15:58:16 -0500

    Proyecto con explicacion

--- front-end/miapp/src/App.vue
+++ front-end/miapp/src/App.vue
@@ -1,7 +1,12 @@
+<!----template: es donde va el HTML que se mostrará en pantalla.-->
 <template>
+  <!--<Productos />: es tu componente (el que creaste en src/components/Productos.vue).-->
   <Productos />
 </template>
 
+<!--script setup: indica que estás usando la sintaxis moderna de Vue 3, donde puedes importar y usar directamente sin hacer export default-->
 <script setup>
+//aquí estás importando tu componente Productos.vue para poder usarlo dentro del <template>.
+//./components/Productos.vue indica la ruta donde está guardado tu componente (en la carpeta components)
 import Productos from './components/Productos.vue';
 </script>
